d3d1c890c1c2836881e23cf62bd4af7b5a9c2995
maxzhang1985 <maxzhang1985@gmail.com>
2015-11-11 13:50:48 +0800

    init project

---
+++ OwinHost/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwinHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (Microsoft.Owin.Hosting.WebApp.Start<Startup>("http://localhost:9000"))
+            {
+                Console.WriteLine("Press [enter] to quit...");
+                Console.ReadLine();
+            }
+
+
+        }
+    }
+}
---
+++ OwinHost/Startup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Owin;
+
+namespace OwinHost
+{
+    public class Startup
+    {
+
+        public void Configuration(IAppBuilder builder)
+        {
+            builder.Run(context =>
+            {
+                context.Response.ContentType = "text/plain";
+                return context.Response.WriteAsync("Hello, world.");
+            });
+        }
+
+
+
+    }
+}
