e410bca8622178b780592afefa48965ded40a999
maxzhang1985 <maxzhang1985@gmail.com>
2015-11-23 14:40:24 +0800

    修改QueryString key value形式

--- YOYO.Owin/QueryString.cs
+++ YOYO.Owin/QueryString.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Collections.Concurrent;
 using System.Linq;
 using System.Text;
 using YOYO.Owin.Helper;
@@ -7,47 +8,44 @@ namespace YOYO.Owin
 {
     public class QueryString
     {
-        private readonly IDictionary<string, string[]> _parts;
+        private readonly IDictionary<string, string> _parts;
 
         public QueryString(string value) {
             _parts = Parse(value);
         }
 
-        public QueryString(IDictionary<string, string[]> parts) {
+        public QueryString(IDictionary<string, string> parts) {
             _parts = parts;
         }
 
-        public IDictionary<string, string[]> Parts {
+        public IDictionary<string, string> Parts {
             get { return _parts; }
         }
 
         public override string ToString() {
             var query = new StringBuilder();
-            foreach (var part in Parts) {
-                foreach (var value in part.Value) {
-                    query.AppendFormat("{0}{1}{2}&", part.Key, value == string.Empty ? string.Empty : "=", UrlHelper.Encode(value));
-                }
-            }
+            foreach (var part in Parts) 
+                query.AppendFormat("{0}{1}{2}&", part.Key, part.Value == string.Empty ? string.Empty : "=", UrlHelper.Encode(part.Value));
+
             if (query.Length > 0) {
                 query.Remove(query.Length - 1, 1);
             }
             return query.ToString();
         }
 
-        private static IDictionary<string, string[]> Parse(string queryString) {
+        private static IDictionary<string, string> Parse(string queryString) {
             if (queryString == string.Empty) {
-                return new Dictionary<string, string[]>();
+                return new ConcurrentDictionary<string, string>();
             }
-            var workingDictionary = new Dictionary<string, List<string>>();
+            var workingDictionary = new ConcurrentDictionary<string, string>();
             var chunks = queryString.Split('&');
             foreach (var chunk in chunks) {
                 var parts = chunk.Split('=');
                 if (!workingDictionary.ContainsKey(parts[0])) {
-                    workingDictionary.Add(parts[0], new List<string>());
+                    workingDictionary.TryAdd(parts[0], UrlHelper.Decode(parts[1]));
                 }
-                workingDictionary[parts[0]].Add(parts.Length == 2 ? UrlHelper.Decode(parts[1]) : string.Empty);
             }
-            return workingDictionary.ToDictionary(kvp => kvp.Key, kvp => kvp.Value.ToArray());
+            return workingDictionary;
         }
 
         public static implicit operator QueryString(string raw) {
