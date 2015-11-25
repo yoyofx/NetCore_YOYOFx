97a9aaeafd70894daf9e25412146a8702f1bf3e4
maxzhang <maxzhang1985@gmail.com>
2015-11-25 22:08:27 +0800

    * YOYOFx.sln: * Test.cs: * YOYOFx.Test.csproj: * YOYO.NUnitTest.csproj:
    * packages.config: add nunit test projects

---
+++ YOYO.NUnitTest/Test.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using YOYOFx.Mvc.Route;
+
+namespace YOYO.NUnitTest
+{
+	[TestFixture ()]
+	public class Test
+	{
+		[Test ()]
+		public void TestCase ()
+		{
+			RouteRole role = new RouteRole ("/{controller}/{action}/id");
+			role.GetRouteValues (null);
+
+
+		}
+	}
+}
+
