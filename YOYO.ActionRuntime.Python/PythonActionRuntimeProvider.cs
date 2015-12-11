48b3645e9bbe90d0422dffda963427fc3059ed1c
maxzhang1985 <maxzhang1985@gmail.com>
2015-12-11 17:00:53 +0800

    add python

---
+++ YOYO.ActionRuntime.Python/PythonActionRuntimeProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YOYO.Mvc.ActionRuntime;
+
+namespace YOYO.ActionRuntime.Python
+{
+    public class PythonActionRuntimeProvider : IActionRuntimeProvider
+    {
+        public object ExecuteAsync(string controllerName, string actionName, params object[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LoadRuntime(string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
