4b244025d534b0b7cd57bd41bd1a83277e977bcd
maxzhang1985 <maxzhang1985@gmail.com>
2016-01-13 16:59:24 +0800

    添加Session功能

---
+++ YOYO.Mvc/Session/Session.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YOYO.Mvc.Session
+{
+    public class Session
+    {
+        public object this[string key]
+        {
+            get
+            {
+                object obj = null;
+                this._userData.TryGetValue(key, out obj);
+                return obj;
+            }
+            set
+            {
+                this._userData[key] = value;
+            }
+        }
+
+        private IDictionary<string, object> _userData = new Dictionary<string, object>();
+
+        internal DateTime ActiveTime { get; set; }
+    }
+}
---
+++ YOYO.Mvc/Session/SessionsStore.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+
+namespace YOYO.Mvc.Session
+{
+    internal class SessionsStore
+    {
+        private  IDictionary<string, Session> sessionStore = new ConcurrentDictionary<string,Session>();
+
+        internal Session GetSession(string sessionid)
+        {
+            Session session = null;
+
+            sessionStore.TryGetValue(sessionid,out session);
+
+            return session;
+        }  
+
+        internal void SetSession(string id, Session session)
+        {
+            if (sessionStore.ContainsKey(id))
+                sessionStore[id] = session;
+            else
+                sessionStore.Add(id, session);
+        }
+
+    }
+}
