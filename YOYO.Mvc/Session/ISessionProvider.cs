312d468d569c468b1b5a765f88e1bd27f3a647b6
张磊 <zl.hxd@hotmail.com>
2016-01-15 15:24:11 +0800

    session provider interface

---
+++ YOYO.Mvc/Session/ISessionProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YOYO.Owin;
+
+namespace YOYO.Mvc.Session
+{
+    internal interface ISessionProvider
+    {
+        ISession AccessSession(IOwinContext context);
+
+    }
+}
