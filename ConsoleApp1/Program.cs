7a7b9e216998baf418127760de0bfa338d1dcd7b
张磊 <zl.hxd@hotmail.com>
2016-05-21 21:28:41 +0800

    test RazorViewEngine by AspNet Core impi

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YOYO.AspNetCore.ViewEngine.Razor;
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            RazorViewEngine.Compile<UserInfo>("<p>@Model.Name</p>");
+
+        }
+
+
+
+
+
+    }
+
+    public class UserInfo
+    {
+        public string Name { set; get; }
+    }
+
+
+}
--- YOYO.AspNetCore.ViewEngine.Razor/RazorViewEngine.cs
+++ YOYO.AspNetCore.ViewEngine.Razor/RazorViewEngine.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Razor;
 using YOYO.Mvc;
+using Microsoft.AspNetCore.Razor.CodeGenerators;
+using Microsoft.AspNetCore.Razor.Compilation;
+using Microsoft.AspNetCore.Razor.Parser;
 
 namespace YOYO.AspNetCore.ViewEngine.Razor
 {
@@ -16,10 +20,6 @@ namespace YOYO.AspNetCore.ViewEngine.Razor
 
 
             var host = new RazorEngineHost(new CSharpRazorCodeLanguage());
-
-            // Act
-            var engine = new RazorTemplateEngine(host);
-
 
 
         }
@@ -44,5 +44,126 @@ namespace YOYO.AspNetCore.ViewEngine.Razor
 
             throw new NotImplementedException();
         }
+
+
+
+
+        public static Type Compile<T>(string template_path)
+        {
+            //准备临时类名，读取模板文件和Razor代码生成器
+            var class_name = "c" + Guid.NewGuid().ToString("N");
+            var base_type = typeof(TemplateBase<>).MakeGenericType(typeof(T));
+            //var template = File.ReadAllText(template_path);
+
+            var host = new RazorEngineHost(new CSharpRazorCodeLanguage(), () => new HtmlMarkupParser())
+            {
+
+                DefaultBaseClass = base_type.FullName,
+                DefaultClassName = class_name,
+                DefaultNamespace = "YourNameSpace.dynamic",
+                GeneratedClassContext =
+                                   new GeneratedClassContext("Execute", "Write", "WriteLiteral", "WriteTo",
+                                                             "WriteLiteralTo",
+                                                             "YourNameSpace.TemplateBase",new GeneratedTagHelperContext())
+
+            };
+            host.NamespaceImports.Add("System");
+            host.NamespaceImports.Add("YourNameSpaces");
+
+            var engine = new RazorTemplateEngine(host);
+
+
+
+            var gc = engine.GenerateCode(new StringReader(template_path));
+
+            //编译成功后， 返回编译后的动态Type
+            return null;
+
+        }
+
+
+
+
+    }
+
+
+
+
+    public abstract class TemplateBase
+    {
+        public string Layout { get; set; }
+ 
+        public Func<string> RenderBody { get; set; }
+        public string Path { get; internal set; }
+        public string Result { get { return Writer.ToString(); } }
+
+        protected TemplateBase()
+        {
+        }
+
+        public TextWriter Writer
+        {
+            get
+            {
+                if (writer == null)
+                {
+                    writer = new StringWriter();
+                }
+                return writer;
+            }
+            set
+            {
+                writer = value;
+            }
+        }
+
+        private TextWriter writer;
+
+        public void Clear()
+        {
+            Writer.Flush();
+        }
+
+        public virtual void Execute() { }
+
+        public void Write(object @object)
+        {
+            if (@object == null)
+            {
+                return;
+            }
+            Writer.Write(@object);
+        }
+
+        public void WriteLiteral(string @string)
+        {
+            if (@string == null)
+            {
+                return;
+            }
+            Writer.Write(@string);
+        }
+
+        public static void WriteLiteralTo(TextWriter writer, string literal)
+        {
+            if (literal == null)
+            {
+                return;
+            }
+            writer.Write(literal);
+        }
+
+        public static void WriteTo(TextWriter writer, object obj)
+        {
+            if (obj == null)
+            {
+                return;
+            }
+            writer.Write(obj);
+        }
+    }
+    public abstract class TemplateBase<T> : TemplateBase
+    {
+        public T Model { get; set; }
     }
 }
