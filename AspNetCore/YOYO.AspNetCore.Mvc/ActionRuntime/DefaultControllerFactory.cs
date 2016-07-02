958aedd7a04dc602503aab5231f9ec76519cb381
张磊 <zl.hxd@hotmail.com>
2016-07-02 23:38:24 +0800

    添加默认的控制器创建工厂，支持IOC注入服务。

--- AspNetCore/YOYO.AspNetCore.Mvc/ActionRuntime/DefaultControllerFactory.cs
+++ AspNetCore/YOYO.AspNetCore.Mvc/ActionRuntime/DefaultControllerFactory.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
 
 namespace YOYO.Mvc.ActionRuntime
 {
@@ -11,11 +12,22 @@ namespace YOYO.Mvc.ActionRuntime
 
         public Controller CreateController(Type controllerType , IServiceProvider serverProvider)
         {
+            Controller activeController = null;
+
+            var constructors = controllerType.GetConstructors();
+            if(constructors.Length > 0 )
+            {
+
+            }
+            else
+            {
+                activeController = (Controller)Activator.CreateInstance(controllerType);
+
+            }
+          
             
 
-
-            var controller = (Controller)Activator.CreateInstance(controllerType);
-            return controller;
+            return activeController;
         }
     }
 }
